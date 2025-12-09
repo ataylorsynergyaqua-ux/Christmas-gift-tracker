26e1bd2dd1346e67e2f5dfa1f8302688da13fe6d
Anastasia Taylor <ataylor.synergyaqua@gmail.com>
2025-12-09 07:11:39 -0600

    Fix NextAuth route and install next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { ServiceWorkerProvider } from "./Components/ServiceWorkerProvider";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ServiceWorkerProvider />
-        {children}
+        {/* All client-only stuff (like SessionProvider) is inside Providers */}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
